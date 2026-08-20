b40177538b41d8bced027a1a8cafd52388e78a56
vinit sharma <sharmavinit54538@gmail.com>
2026-08-20 19:50:12 +0530

    feat: export payroll types, queries, and mutations from new payrollApi module

--- src/services/api/payrollApi.ts
+++ src/services/api/payrollApi.ts
@@ -1,5 +1,3 @@
-import { payrollQueriesApi } from "./payrollQueries";
 export * from "./payrollTypes";
 export * from "./payrollQueries";
 export * from "./payrollMutations";
-export const payrollApi = payrollQueriesApi;
